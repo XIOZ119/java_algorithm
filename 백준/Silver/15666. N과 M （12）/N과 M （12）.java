d0fa910a79bf223e52dd94906773c1aa550a3ab0
Hong sieun <63907578+XIOZ119@users.noreply.github.com>
2025-02-26 16:30:08 +0900

    [Silver II] Title: N과 M (12), Time: 152 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	private static int N, M;
+	private static int[] arr;
+	private static int[] result;
+	private static BufferedWriter bw;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		result = new int[M];
+		
+		String strr = br.readLine();
+		st = new StringTokenizer(strr);
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		dfs(0);
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	private static void dfs(int depth) throws IOException {
+		int before = -1;
+		
+		if(depth == M) {
+			for(int re: result) {
+				bw.write(re + " ");
+			}
+			bw.write("\n");
+			
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			int d=0;
+			
+			if(depth > 0) {
+				d = result[depth-1];
+			}
+			if(before != arr[i]) {
+				if(d <= arr[i]) {
+					result[depth] = arr[i];
+					before = arr[i];
+					dfs(depth+1);
+				}
+			}
+		}
+	}
+
+}
