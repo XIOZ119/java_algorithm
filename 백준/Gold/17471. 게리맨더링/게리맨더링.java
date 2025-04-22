2ad8cb228ee345d4a4f943479de602a5f31b4143
Hong sieun <63907578+XIOZ119@users.noreply.github.com>
2025-04-22 15:41:06 +0900

    [Gold III] Title: 게리맨더링, Time: 124 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,128 +1,124 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static ArrayList<Integer>[] graph;
-    static int answer;
+	static int N;
+	static int[] arr;
+	static List<Integer>[] graph;
+	static int result;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N+1];
+		N = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
 
-        for(int i = 1; i < N+1; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        graph = new ArrayList[N+1];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        for(int i=0; i<N+1; i++) {
-            graph[i] = new ArrayList<>();
-        }
+		graph = new ArrayList[N];
 
-        for(int i=1; i<N+1; i++) {
-            st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			graph[i] = new ArrayList<>();
+		}
 
-            int m = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int cnt = Integer.parseInt(st.nextToken());
 
-            // 양방향 그래프
-            for(int j=0; j<m; j++) {
-                int a = Integer.parseInt(st.nextToken());
-                graph[i].add(a);
-                graph[a].add(i);
-            }
-        }
+			for (int j = 0; j < cnt; j++) {
+				int node = Integer.parseInt(st.nextToken());
 
-        answer = Integer.MAX_VALUE;
-        comb(new boolean[N+1], 0, 0);
-        
-        if(answer == Integer.MAX_VALUE){
-            bw.write("-1");
-        } else {
-            bw.write(answer + "");
-        }
-        bw.flush();
-        bw.close();
-    }
-    
-    private static void comb(boolean[] color, int count, int start) {
+				graph[i].add(node - 1);
+				graph[node - 1].add(i);
+			}
+		}
 
-        if(count == N) return;
+		// 조합 -> 연결되었는지 판단하기
+		result = Integer.MAX_VALUE;
+		comb(new boolean[N], 0, 0);
 
-        // 그래프로 이어진 곳인지 확인하기
-        if(count != 0){
-            if(linked(color)){
-                int red = 0;
-                int blue = 0;
-                for(int i=1; i<N+1; i++) {
-                    if(color[i]) red += arr[i];
-                    else blue += arr[i];
-                }
-    
-                int r = Math.abs(red - blue);
-                answer = Math.min(answer, r);
-            }
-        }
-        
+		if (result == Integer.MAX_VALUE) {
+			bw.write("-1");
+		} else {
+			bw.write(result + "");
+		}
+		bw.flush();
+		bw.close();
+	}
 
-        for(int i=start; i<N+1; i++) {
-            if(!color[i]){
-                color[i] = true;
-                comb(color, count+1, i+1);
-                color[i] = false;
-            }
-        }
-    }
+	private static void comb(boolean[] color, int count, int start) {
 
-    private static boolean linked(boolean[] color){
-        boolean red = true, blue = true;
-        int r = 0, b = 0;
+		if (count != 0 && count != N) {
+			// 연결 확인
+			boolean[] red = new boolean[N];
+			int r = 0;
+			boolean[] blue = new boolean[N];
+			int b = 0;
 
-        for(int i=1; i<N+1; i++) {
-            if(color[i] && r == 0) {
-                red = isConnected(i, color);
-                r++;
-            }
-            if(!color[i] && b == 0) {
-                blue = isConnected(i, color);
-                b++;
-            }
-        }
-        
-        return red && blue;
-    }
+			int sumRed = 0;
+			int sumBlue = 0;
 
-    private static boolean isConnected(int start, boolean[] color) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[N+1];
-        
-        queue.add(start);
-        visited[start] = true;
+			for (int i = 0; i < N; i++) {
+				if (color[i]) {
+					red[i] = true;
+					r = i;
+					sumRed += arr[i];
+				} else {
+					blue[i] = true;
+					b = i;
+					sumBlue += arr[i];
+				}
+			}
 
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
+			if (isConnected(red, r) && isConnected(blue, b)) {
+				result = Math.min(result, Math.abs(sumRed - sumBlue));
+			}
+		}
 
-            for(int c: graph[cur]){
-                if(color[c] != color[start] || visited[c]) continue;
+		if (count == N)
+			return;
 
-                queue.add(c);
-                visited[c] = true;
-            }
-        }
+		for (int i = start; i < N; i++) {
+			if (!color[i]) {
+				color[i] = true;
+				comb(color, count + 1, i + 1);
+				color[i] = false;
+			}
+		}
+	}
 
-        for(int i=1; i<N+1; i++) {
-            if(color[i] == color[start]) {
-                if(!visited[i]) return false;
-            }
-        }
+	private static boolean isConnected(boolean[] selected, int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		boolean[] visited = new boolean[N];
 
-        return true;
-    }
+		queue.add(start);
+		visited[start] = true;
+
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+
+			for (int c : graph[cur]) {
+				if (!visited[c] && selected[c]) {
+					visited[c] = true;
+					queue.add(c);
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (selected[i]) {
+				if (!visited[i])
+					return false;
+			}
+		}
+
+		return true;
+	}
 }
