09c28b36b8247fe4c358d8ae2a16f38a15fa2523
Hong sieun <tjwns53@naver.com>
2025-04-27 16:04:44 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 324 ms, Memory: 36260 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,81 +1,87 @@
-import java.util.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 public class Main {
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static boolean[][] visited;
-	static char[][] arr;
-	static int startX, startY, endX, endY;
-	static int N, M;
-	static int result;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new char[N][M];
-		visited = new boolean[N][M];
-		
-		startX = 0; startY = 0; 
-		endX = 0; endY = 0;
-		
-		for(int i=0; i<N; i++) {
-			String str = br.readLine();
-			for(int j=0; j<M; j++) {
-				arr[i][j] = str.charAt(j);
-				if(arr[i][j] == 'I') {
-					startX = i;
-					startY = j;
-				}
-			}
-		}
-		
-		result = 0;
-		bfs();
-		
-		if(result == 0) bw.write("TT");
-		else bw.write(result + "");
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	private static void bfs() {
-		Queue<int []> queue = new LinkedList<>();
-		
-		queue.add(new int[] {startX, startY});
-		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			
-			int cx = cur[0];
-			int cy = cur[1]; 
-			
-			for(int i=0; i<4; i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
-				
-				if(isValid(nx, ny) && !visited[nx][ny]) {
-					if(arr[nx][ny] == 'P') {
-						result++;
-					}
-					if(arr[nx][ny] != 'X') {
-						visited[nx][ny] = true;
-						queue.add(new int[] {nx, ny});
-					}
-				}
-			}
-			
-		}
-	}
-	
-	private static boolean isValid(int x, int y) {
-		return x > -1 && y > -1 && x < N && y < M;
-	}
-}
+
+    /*
+     * 3 5  
+     * OOOPO
+     * OIOOX
+     * OOOXP
+     * O는 빈 공간, X는 벽, I는 도연이, P는 사람
+     */
+
+    static int r, c;
+    static char[][] arr;
+    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        arr = new char[r][c];
+        int startX = 0; 
+        int startY = 0;
+
+        for(int i=0; i<r; i++) {
+            String str = br.readLine();
+            for(int j=0; j<c; j++) {
+                arr[i][j] = str.charAt(j);
+                if(arr[i][j] == 'I') {
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
+
+        int result = bfs(startX, startY);
+
+        if(result == 0 ){
+            bw.write("TT");
+        } else {
+            bw.write(result + "");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static int bfs(int x, int y){
+        boolean[][] visited = new boolean[r][c];
+        Queue<int[]> queue = new LinkedList<>();
+        int meet = 0;
+
+        visited[x][y] = true;
+        queue.add(new int[] {x, y});
+
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for(int i=0; i<4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                
+                if(!isValid(nx, ny) || visited[nx][ny]) continue;
+                if(arr[nx][ny] == 'X') continue;
+            
+                if(arr[nx][ny] == 'P') meet++;
+                visited[nx][ny] = true;
+                queue.add(new int[] {nx, ny});
+            }
+        }
+        
+        return meet;
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < r && y < c;
+    }
+}  
