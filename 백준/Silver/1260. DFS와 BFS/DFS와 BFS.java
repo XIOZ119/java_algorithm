7c0c36cfe0a13b253b0c225bc0433f9860741f8d
Hong sieun <tjwns53@naver.com>
2025-08-09 00:29:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 228 ms, Memory: 20576 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,84 +3,79 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    private static ArrayList<Integer>[] arr;
-    private static boolean[] visited;
-    private static String result = "";
+    static List<Integer>[] arr;
+    static boolean[] visited;
+    static StringBuilder sb;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
 
-        String[] inputConditino = br.readLine().split(" ");
-        int N = Integer.parseInt(inputConditino[0]);
-        int M = Integer.parseInt(inputConditino[1]);
-        int start = Integer.parseInt(inputConditino[2]);
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
 
-        arr = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        for(int i = 1; i <= N; i++ ){
+        arr = new ArrayList[n+1];
+
+        for(int i=0; i<n+1; i++){
             arr[i] = new ArrayList<>();
         }
 
-        for(int i=1; i<= M; i++){
-            String[] inputNode = br.readLine().split(" ");
-            int a = Integer.parseInt(inputNode[0]);
-            int b = Integer.parseInt(inputNode[1]);
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
             arr[a].add(b);
             arr[b].add(a);
         }
 
-        // 정렬 (작은 수부터 탐색하기 위해)
-        for(int i=1; i<= N; i++) {
+        for(int i=1; i<n+1; i++){
             Collections.sort(arr[i]);
         }
 
-        //DFS 연결
-        dfs(start);
-        bw.write(result);
-        bw.write("\n");
-
-        result = "";
-        visited = new boolean[N+1];
+        visited = new boolean[n+1];
+        dfs(v);
         
-        //BFS 연결
-        bfs(start);
-        bw.write(result);
+        sb.append("\n");
+
+        visited = new boolean[n+1];
+        bfs(v);
+        
+        bw.write(sb.toString());
         bw.flush();
     }
 
-    private static void dfs(int node) {
-        visited[node] = true;
-        result += node + " ";
-        for(int child: arr[node]) {
-            if(!visited[child]){
-                dfs(child);
-            }
+    static void dfs(int start){
+        visited[start] = true;
+        sb.append(start + " ");
+
+        for(int a: arr[start]){
+            if(visited[a]) continue;
+
+            dfs(a);
         }
     }
 
-    private static void bfs(int node) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        // 큐에 시작 노드 삽입 (add 연산)
-        queue.add(node);
-        // visited 배열에 현재 노드 방문 기록하기
-        visited[node] = true;
-        
-        // while (큐가 비어 있을 때까지)
-        while(!queue.isEmpty()) {
-            // 큐에서 노드 데이터 가져오기 (poll 연산)
-            int now_node = queue.poll();
-            // 가져온 노드 출력하기 
-            result += now_node + " ";
-            // 현재 노드의 연결 노드 중 미방문 노드를 큐에 삽입 (add 연산) 하고 방문 배열에 기록하기 
-            for(int child: arr[now_node]) {
-                if(!visited[child]){
-                    queue.add(child);
-                    visited[child] = true;
-                }
+    static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            sb.append(cur + " ");
+            
+            for(int next: arr[cur]){
+                if(visited[next]) continue;
+
+                visited[next] = true;
+                queue.add(next);
             }
         }
-        
     }
 }
