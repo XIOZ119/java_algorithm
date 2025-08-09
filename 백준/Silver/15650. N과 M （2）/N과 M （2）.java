124144635e6cf137f9af17085f1955f7fe2c525f
Hong sieun <tjwns53@naver.com>
2025-08-09 17:22:34 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,54 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	private static int N;
-	private static int M;
-	private static int[] arr;
-	private static boolean[] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str);
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[M];
-		visited = new boolean[N];
-		
-		dfs(0);
-	}
-	
-	private static void dfs(int depth) {
-		
-		if(depth == M) {
-			for(int result: arr) {
-				System.out.print(result + " ");
-			}
-			System.out.println();
-			
-			return;
-		}
-		
-		for(int i=0; i<N; i++) {
-			
-			if(!visited[i]) {
-				int d = 0;
-				if(depth > 0) {
-					d = depth - 1; 					
-				}
-				if(arr[d] < i+1){				
-					visited[i] = true;
-					arr[depth] = i + 1;
-					dfs(depth+1);
-					
-					visited[i] = false;
-				}
-			}
-		}
-	}
+class Main {
+    static StringBuilder sb;
+    static boolean[] visited;
+    static int[] record;
+    static int n, m;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n+1];
+        record = new int[m];
+
+        perm(1, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void perm(int start, int depth) {
+        if(depth == m){
+            for(int i=0; i<m; i++){
+                sb.append(record[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<n+1; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                record[depth] = i;
+                perm(i+1, depth+1);
+                visited[i] = false;
+            }
+        }
+    }
 }
