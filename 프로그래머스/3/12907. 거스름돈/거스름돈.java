a2798988729045ce6250b34957a3796011080f84
Hong sieun <tjwns53@naver.com>
2025-12-24 16:08:59 +0900

    [level 3] Title: 거스름돈, Time: 9.95 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int i=0; i<money.length; i++) {
+            int coin = money[i];
+            for(int j=coin; j<=n; j++) {
+                dp[j] += dp[j-coin];
+            }
+        }
+        
+        return dp[n];
+    }
+}
