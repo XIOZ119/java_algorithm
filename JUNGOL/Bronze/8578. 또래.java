21b4285fffd4be4b62d35aa14d02093c5625913c
Hong sieun <tjwns53@naver.com>
2026-07-10 18:37:47 +0900

    Add program to calculate minimum difference in array
    Implement a program to find the minimum difference between consecutive numbers in a sorted array.

---
+++ JUNGOL/Bronze/8578. 또래.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = Integer.MAX_VALUE;
+        for(int i=1; i<N; i++) {
+            int min = arr[i] - arr[i-1];
+
+            answer = Math.min(answer, min);
+        }
+
+        System.out.println(answer);
+    }
+}
