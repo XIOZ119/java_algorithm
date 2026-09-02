1b4061abb5fdd1b702869cc5d4049d05faefec00
Hong sieun <tjwns53@naver.com>
2026-09-02 14:46:19 +0900

    [level 2] Title: 점 찍기, Time: 6.18 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for(long x = 0; x <= d; x += k) {
+            long maxY = (long) Math.sqrt((long) d * d - x * x);
+            
+            long count = maxY / k + 1;
+            
+            answer += count;
+        }
+        
+        return answer;
+    }
+}
